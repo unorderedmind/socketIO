0ab29a51ee467a3dba5c7eb71f26a1d4ccd921cb
shojib <shoojibbd@gmail.com>
2017-12-26 23:29:46 +0100

    second commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('bodyParser');
+const ejs = require('ejs');
+const http = require('http');
+const container = require('./container');
+
+
+
+container.resolve(function(users) {
+
+  const app = SetupExpress();
+
+  function SetupExpress(){
+      const app = express();
+      const server = http.createServer(app);
+      server.listen(3000, function(){
+          console.log('Listening on port 3000');
+      });
+      ConfigureExpress(app);
+
+      //Setup router
+      const router = require('express-promise-router')();
+      users.SetRouting(router);
+
+      app.use(router);
+    }
+
+
+
+    function ConfigureExpress(app){
+        app.use(express.static('public'));
+        app.set('view engine', 'ejs');
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: true}));
+    }
+});
